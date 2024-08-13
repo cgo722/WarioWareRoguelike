d7581cb0a5ef8be68838fe3859bbae76dbe69453
Camron <holy_hades@ymail.com>
2024-08-12 20:04:35 -0600

    Fixed a couple of small bugs

--- PartyGameRougeLike/Scripts/Tag/Tag3DManager.gd
+++ PartyGameRougeLike/Scripts/Tag/Tag3DManager.gd
@@ -11,6 +11,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if $StandardCharacter3D.position.y < -15:
+		gamemanager.death = true
 	pass
 
 
