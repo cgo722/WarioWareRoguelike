c59fd201e309b4e0229fc7372564c059789131b4
Camron <holy_hades@ymail.com>
2024-08-28 10:43:24 -0600

    Decided to scrap car idea

--- PartyGameRougeLike/Scenes/MicroGames/Car.gd
+++ PartyGameRougeLike/Scenes/MicroGames/Car.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 @export var speed : float
+var curSpeed : float
 @export var minX : float
 @export var maxX : float
 @onready var player := $"../StandardPlayer"
@@ -12,15 +13,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if position.x < minX :
+		speed *= randf_range(0.1, 2)
 		speed *= -1
+		flip_h = true
 	if position.x > maxX:
-		speed *= -1
+		speed *= randf_range(0.1, 2)
+		flip_h = false
 	
-	position.x += speed
+	clampf(curSpeed, -20, 20)
+	curSpeed = speed
+	position.x += curSpeed
 	pass
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	player.queue_free()
-	print("I killed you")
 	pass # Replace with function body.
