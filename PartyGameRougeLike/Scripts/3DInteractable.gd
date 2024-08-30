45cfaaea92a352bdc7692e630275d09c06723858
CamronLinux <holy_hades@ymail.com>
2024-08-30 13:39:24 -0600

    finished main look of all of the levels just working on spicing it up

--- PartyGameRougeLike/Scripts/3DInteractable.gd
+++ PartyGameRougeLike/Scripts/3DInteractable.gd
@@ -5,7 +5,7 @@ var bag = []
 var ranPos
 var locations = [Vector3(-0.2, 7.2, -13.9), Vector3(12.6, 7.8, -19.9), Vector3(22.3, 6.4, -4.5), Vector3(-6.4, 9.1, -2.5), Vector3(-11.7, 1.9, -1.9)]
 @onready var anim = $AnimationPlayer
-@onready var spawnParticles := $SpawnParticles
+@onready var spawnParticles := $"3DSmokeClouds"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Bag()
