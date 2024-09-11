f51591e0575fa5af6d84e6056380a0f5de17edd8
Camron <holy_hades@ymail.com>
2024-09-11 11:13:46 -0600

    Working on creating a music player UI

---
+++ PartyGameRougeLike/Scripts/UI/music_display.gd
@@ -0,0 +1,42 @@
+extends BoxContainer
+
+@onready var title: Label = $NinePatchRect/VSplitContainer/Title
+@onready var artist: Label = $NinePatchRect/VSplitContainer/Artist
+@onready var audio_stream_player: AudioStreamPlayer = $"../AudioStreamPlayer"
+var Titles := ["Alien Ballad", "Synth Ballad 05", "Your Digital FootPrint", "Unkown (Hideout Theme)", "Shattering the Illustion"]
+var Artists := ["Oakie Studios LLC", "One Man Symphony"]
+@onready var playlist = audio_stream_player.stream
+func _ready() -> void:
+	if playlist.get_list_stream(0):
+		title.text = Titles[0]
+		artist.text = Artists[0]
+	if playlist.get_list_stream(1):
+		title.text = Titles[1]
+		artist.text = Artists[1]
+	if playlist.get_list_stream(2):
+		title.text = Titles[2]
+		artist.text = Artists[2]
+	if playlist.get_list_stream(3):
+		title.text = Titles[3]
+		artist.text = Artists[3]
+	if playlist.get_list_stream(4):
+		title.text = Titles[4]
+		artist.text = Artists[4]
+		
+func _on_audio_stream_player_finished() -> void:
+	if playlist.get_list_stream(0):
+		title.text = Titles[0]
+		artist.text = Artists[0]
+	if playlist.get_list_stream(1):
+		title.text = Titles[1]
+		artist.text = Artists[1]
+	if playlist.get_list_stream(2):
+		title.text = Titles[2]
+		artist.text = Artists[2]
+	if playlist.get_list_stream(3):
+		title.text = Titles[3]
+		artist.text = Artists[3]
+	if playlist.get_list_stream(4):
+		title.text = Titles[4]
+		artist.text = Artists[4]
+	pass # Replace with function body.
