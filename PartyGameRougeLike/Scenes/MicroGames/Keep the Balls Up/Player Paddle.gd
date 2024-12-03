2e8601d52c0b0ebfcf84d20a6204a8abfbee05ac
Camron <holy_hades@ymail.com>
2024-12-03 11:39:12 -0700

    I finished the main gameplay loop for the keep the balls up. I just need to add functionality with the roguelike elements and add it to the loop of games and add art

--- PartyGameRougeLike/Scenes/MicroGames/Keep the Balls Up/Player Paddle.gd
+++ PartyGameRougeLike/Scenes/MicroGames/Keep the Balls Up/Player Paddle.gd
@@ -1,12 +1,17 @@
-extends RigidBody3D
+extends AnimatableBody2D
 
 @export var speed : float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	apply_central_impulse(basis.z * -speed)
+	#speed *= gamemanager.playerSpeed2d
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var dir = Input.get_axis("ui_left", "ui_right")
+	if dir != 0:
+		position.x += (dir * speed)
+	else:
+		dir = 0
 	pass
--- PartyGameRougeLike/Scenes/MicroGames/Keep the Balls Up/ball.gd
+++ PartyGameRougeLike/Scenes/MicroGames/Keep the Balls Up/ball.gd
@@ -1,19 +1,29 @@
 extends RigidBody2D
 
-
+var speed : float = 1000
+var vel : float = 750
+var down : bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	down = true
+	#angular_velocity = randf_range(-180, 180)
+	apply_central_impulse(Vector2(0,speed))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if down == true:
+		if linear_velocity.y < vel:
+			apply_central_force(Vector2(0,speed))
+	else:
+		if linear_velocity.y > -vel:
+			apply_central_force(Vector2(0,-speed))
 	pass
 	
 
 
-
 func _on_body_entered(body: Node) -> void:
-	var vel : Vector2 = get_linear_velocity()
-	set_linear_velocity(vel * -1)
+	apply_central_impulse(Vector2(randf_range(-350,350), 0))
+	down = not down
 	pass # Replace with function body.
